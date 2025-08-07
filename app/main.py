b9895b9b342676d67a0d7742c20bfaae1712419b
Ruslan B <bondarau.ruslan@arelion.com>
2025-08-07 23:16:43 +0200

    add static

--- app/main.py
+++ app/main.py
@@ -1,5 +1,11 @@
 from fastapi import FastAPI
 from services.aggregator import router as product_router
+from fastapi.staticfiles import StaticFiles
+import os
+
+
+frontend_path = os.path.join(os.path.dirname(__file__), "frontend")
+app.mount("/frontend", StaticFiles(directory=frontend_path, html=True), name="frontend")
 
 app = FastAPI(title="Grocery Store API")
 
